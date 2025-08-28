12558a51b08ad6267e0454d0313eeba196f5a040
asuarezgarcia <a.suarezg@uniandes.edu.co>
2025-08-27 21:00:00 -0500

    Poner archivos bases de datos y cargarlas

--- scripts/DescargaDatos.r
+++ scripts/DescargaDatos.r
@@ -16,3 +16,23 @@ rm(list = ls())
 
 require(pacman)
 p_load(rvest, dplyr, tidyr, readr, httr, jsonlite)
+
+
+# -----------------------------------------------------
+# PART A
+# -----------------------------------------------------
+
+
+# Load data for part A and b (relative path)
+parte_a <- readRDS("Parte_A.rds")
+
+# Verify no null data
+colSums(is.na(parte_a))
+
+# -----------------------------------------------------
+# PART B
+# -----------------------------------------------------
+parte_b <- readRDS("Parte_B.rds")
+
+# Verify no null data
+colSums(is.na(parte_b))
