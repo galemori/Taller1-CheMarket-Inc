f0f91e141ff2edc489dffb7e3fef0b6fe5d629c1
asuarezgarcia <a.suarezg@uniandes.edu.co>
2025-08-30 22:21:29 -0500

    Parte A; avance modelos

--- scripts/DescargaDatos.r
+++ scripts/DescargaDatos.r
@@ -26,18 +26,21 @@ p_load(rvest, dplyr, tidyr, readr, httr, jsonlite)
 # Load data for part A and b (relative path)
 parte_a <- readRDS("stores/Parte_A.Rds")
 
-# Verify no null data
+# Verify no null data (dan todos 100k datos)
 colSums(is.na(parte_a))
 
 # Define categoric variable "device_type"
 parte_a <- parte_a %>%
   mutate(device_type = as.factor(device_type)) %>%
   mutate(is_returning_user = as.factor(is_returning_user)) %>%
-  mutate(sign_up = as.factor(sign_up))
+  mutate(sign_up = as.factor(sign_up)) %>%
+  mutate(os_type = as.factor(os_type))
 
+# Poner de base aquella categoría con menor cantidad de observaciones
 parte_a$device_type <- relevel(parte_a$device_type, ref = "tablet")
 
-## Faltaría para OS_type si se agrega al final #TODO
+# Poner de base aquella categoría con menor cantidad de observaciones
+parte_a$device_type <- relevel(parte_a$os_type, ref = "other")
 
 # -----------------------------------------------------
 # PART B
@@ -46,3 +49,12 @@ parte_b <- readRDS("stores/Parte_B.Rds")
 
 # Verify no null data
 colSums(is.na(parte_b))
+
+# Define categoric variable "device_type"
+parte_b <- parte_b %>%
+  mutate(device_type = as.factor(device_type)) %>%
+  mutate(is_returning_user = as.factor(is_returning_user)) %>%
+  mutate(sign_up = as.factor(sign_up)) %>%
+  mutate(os_type = as.factor(os_type))
+
+parte_b$device_type <- relevel(parte_b$device_type, ref = "tablet")
--- scripts/ParteA.r
+++ scripts/ParteA.r
@@ -1,12 +1,12 @@
+
 # -----------------------------------------------------
 # PART 1
 # Exploratory and predictive analysis from observed data
 # 0) Libraries and data
 # 1) Description of users general behavior
 # 2) Estimate effect of "sign_up" into "revenue"
-# 3) Evaluating model's predictive capacity
-# 4) Evaluation: ¿is there correlation or causality?
-# 5)?Should there be an experiment to confirm findings?
+# 3) Logistic regression of variables into sign_up
+# r) Evaluating model's predictive capacity
 # -----------------------------------------------------
 
 # -----------------------------------------------------
@@ -32,7 +32,7 @@ non_categoric_vars <- !categoric_vars
 # -----------------------------------------------------
 # Summary statistics
 summary(db)
-lapply(db[, non_categoric_vars], summary)
+# TODO exportar
 
 # Histograms (Para no categóricas) (#TODO: falta ponerlo bonito)
 histograma_f <-  function(var_name) {
@@ -40,40 +40,112 @@ histograma_f <-  function(var_name) {
 }
 lapply(names(db)[non_categoric_vars], histograma_f)
 
+# Log-Revenue y sqrt-time (vemos que si mejoran y lo cambiamos en la db)
+#TODO: exportar log-rev para anexo
+plot(density(log(db$Revenue)), main = "Density of log-Revenue")
+plot(density(sqrt(db$time_spent)), main = "Density sqrt-time")
+
+db <- db %>% mutate(log_revenue = log(Revenue))
+db <- db %>% mutate(sqrt_time_spent = sqrt(time_spent))
+
+
+### Ya verificamos las variables. Seleccionemos las dependientes del modelo
+
+
+# See pairs (log(revenue) vs continuas) (correlations)
+# TODO exportar
+str <- paste0("~", paste(names(db)[non_categoric_vars], collapse = "+"))
+pairs(db[, names(db)[non_categoric_vars]])
+
 
 # Box-wiskers (Para no categóricas) 
 #TODO no sé si esto lo necesito realmente
+# TODO exportar
 box_f <-  function(var_name) {
   boxplot(db[[var_name]], main = paste("Boxplot of", var_name))
 }
 lapply(names(db)[non_categoric_vars], box_f)
 
-# Log-Revenue
-plot(density(log(db$Revenue)), main = "Density of log-Revenue")
-
-# TODO: Considerar log(time_spent) por el box-plot
-
-# Boxplots (Para categóricas)
-#TODO no sé si esto lo necesito realmente; podría ser solo % de la población total
-lapply(names(db)[categoric_vars], box_f)
-  # Sumar todos los returning; hay 95k / 100k
-  sum(db$is_returning_user == 1, na.rm = TRUE)
-
-  # Sumar los sign_up; haay 77k / 100K
-  sum(db$sign_up == 1, na.rm = TRUE)
-
 # Boxplots categóricas vs revenue
 # Todo: gráficas bien, pero imprime un montón de números 
+# TODO exportar
 box_f_cat_rev <-  function(var_name) {
-  boxplot(log(db$Revenue) ~ db[[var_name]], main = paste("Boxplot of log(Revenue) by", var_name))
+  boxplot(db$log_revenue ~ db[[var_name]], main = paste("Boxplot of log(Revenue) by", var_name))
 }
 lapply(names(db)[categoric_vars], box_f_cat_rev)
 
-# See pairs (log(revenue) vs continuas) (correlations)
-str <- paste0("~", paste(names(db)[non_categoric_vars], collapse = "+"))
-pairs(db[, names(db)[non_categoric_vars]])
+
+
+
+#
+# Función: Boxplot + diferencia de medias
+box_f_cat_rev <- function(var_name) {
+  # Boxplot
+  boxplot(db$log_revenue ~ db[[var_name]], 
+          main = paste("Boxplot of log(Revenue) by", var_name))
+  
+  # Diferencia de medias
+  means <- tapply(db$log_revenue, db[[var_name]], mean, na.rm = TRUE)
+  diffs <- combn(means, 2, FUN = function(x) diff(rev(x))) # diferencias entre categorías
+
+  # Graficar resultados con ggplot2 y ponlo bonito
+  df <- data.frame(Category = names(means), Mean = means)
+  ggplot(df, aes(x = Category, y = Mean)) +
+    geom_bar(stat = "identity") +
+    labs(title = paste("Mean of log(Revenue) by", var_name))
+}
+
+# Aplicar a todas las categóricas
+lapply(names(db)[categoric_vars], box_f_cat_rev)
+
+
+
+
+# Función: medias + IC95% y gráfico con barras + errorbars
+plot_mean_diff <- function(var_name) {
+  
+  # Calcular medias e IC95%
+  summary_df <- db %>%
+    group_by(!!sym(var_name)) %>%
+    summarise(
+      mean_rev = mean(log_revenue, na.rm = TRUE),
+      sd_rev   = sd(log_revenue, na.rm = TRUE),
+      n        = n(),
+      .groups = "drop"
+    ) %>%
+    mutate(
+      se = sd_rev / sqrt(n),
+      ci_low = mean_rev - 1.96 * se,
+      ci_high = mean_rev + 1.96 * se
+    )
+  
+  # Gráfico: barras + error bars en negro
+  ggplot(summary_df, aes(x = !!sym(var_name), y = mean_rev, fill = !!sym(var_name))) +
+    geom_col(width = 0.6, alpha = 0.8) +   # Barras
+    geom_errorbar(aes(ymin = ci_low, ymax = ci_high), 
+                  width = 0.2, color = "black", size = 0.8) +  # Intervalos en negro
+    labs(
+      title = paste("Media de log(Revenue) por", var_name),
+      x = var_name,
+      y = "Mean log(Revenue)"
+    ) +
+    theme_minimal() +
+    theme(legend.position = "none")
+}
+
+# Aplicar la función a todas las variables categóricas
+plots <- lapply(names(db)[categoric_vars], plot_mean_diff)
+
+# Mostrar el primero como ejemplo
+plots[[1]]
+plots[[2]]
+plots[[3]]
+plots[[4]]
+# TODO: exportar
+
 
 # Categorics vs sign_up
+# TODO exportar
 cat_vs_cat <- function(var1, var2) {
   ggplot(db, aes_string(x = var1, fill = var2)) +
     geom_bar(position = "dodge") +
@@ -92,12 +164,14 @@ lapply(names(db)[categoric_vars], function(var1) {
 
 #TODO: si hay tiempo, puedo regresar usando lapply
 # Simple linear regression model
-model <- lm(log(Revenue) ~ sign_up, data = db)
+model <- lm(log_revenue ~ sign_up, data = db)
+# TODO exportar
 summary(model)
 
 # Controlling by all variables
-model_controlled <- lm(log(Revenue) ~ sign_up + time_spent + past_sessions + 
+model_controlled <- lm(log(Revenue) ~ sign_up + sqrt_time_spent + past_sessions + 
                          device_type + is_returning_user + os_type, data = db)
+# TODO exportar
 summary(model_controlled)
 
 # Controlar por interacciones entre vars fuertemente relacionadas a Revenue
@@ -120,8 +194,32 @@ summary(model_controlled_sign)
 
 # Modelo final (quitando aquellas anteriores no significativas)
 
+
+
 # -----------------------------------------------------
-# 3) Evaluating model's predictive capacity
+# 3) Logistic regression of variables into sign_up
+# -----------------------------------------------------
+
+# Quitamos revenue del set de variables explicativas
+vars <- setdiff(names(db), c("sign_up", "revenue"))
+
+# Fórmula dinámica
+fml <- as.formula(paste("sign_up ~", paste(vars, collapse = " + ")))
+
+# Ajustamos el modelo logístico
+logit_model <- glm(fml, data = db, family = binomial(link = "logit"))
+
+# Resumen del modelo
+summary(logit_model)
+
+# Odds ratios en vez de coeficientes logit
+exp(coef(logit_model))
+
+# Intervalos de confianza de los odds ratios
+exp(confint(logit_model))
+
+# -----------------------------------------------------
+# 4) Evaluating model's predictive capacity
 # -----------------------------------------------------
 
 # Fijamos la semilla
@@ -163,14 +261,5 @@ predictor<-function(regresors){
 lapply(models, predictor)
 
 
-
-
-# -----------------------------------------------------
-# 4) Evaluation: ¿is there correlation or causality?
-# -----------------------------------------------------
-
-# -----------------------------------------------------
-# 5) Should there be an experiment to confirm findings?
-# -----------------------------------------------------
 
 
